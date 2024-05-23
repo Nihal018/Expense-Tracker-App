5db3b53cba74297be6d4f1ce0eb62eca130bb959
Nihal018 <nihalchoutapelly@gmail.com>
2024-05-23 20:34:37 +0530

    created and updated expenses-context

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButtons";
+import Button from "../components/UI/Button";
 
 export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -12,10 +13,28 @@ export default function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditting]);
 
-  function deletePressHandler() {}
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function deletePressHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditting ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditting && (
         <View style={styles.deleteContainer}>
           <IconButton
---
+++ store/exoenses-context.tsx
@@ -0,0 +1,71 @@
+import { createContext, useReducer } from "react";
+
+import Expense from "../models/Expense";
+
+export const EXPENSES = [
+  new Expense(1, "Pizza", 9.99, "2024-05-19"),
+  new Expense(2, "Italian Suit", 500, "2024-05-19"),
+  new Expense(3, "Metro Fare", 5, "2024-05-12"),
+  new Expense(4, "Weekly German Classes", 20, "2024-04-09"),
+  new Expense(5, "New Chair", 10, "2024-05-17"),
+  new Expense(6, "Exotic Fruits", 100, "2024-05-18"),
+];
+
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, cost, title }) => {},
+  updateExpense: (id, { title, date, cost }) => {},
+  deleteExpense: (id) => {},
+});
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(ExpensesReducer, EXPENSES);
+
+  function addExpense(expenseData) {
+    dispatch({ type: "Add", payload: expenseData });
+  }
+
+  function deleteExpense(id) {
+    dispatch({ type: "delete", id: id });
+  }
+
+  function updateExpense(id, expenseData) {
+    dispatch({ type: "update", payload: { ...expenseData, id: id } });
+  }
+
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+}
+
+export default ExpensesContextProvider;
